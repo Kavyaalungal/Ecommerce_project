d165b33bca71c69a7722b557848d6b4433c054ba
Kavyaalungal691 <kavyaalungal691@gmail.com>
2023-12-15 13:16:19 +0530

    ecommerce

---
+++ client/src/components/register/Register.jsx
@@ -0,0 +1,35 @@
+function Register(){
+    return (
+        <SignInContainer>
+            <SignInTitle>I already have an account</SignInTitle>
+            <span>Sign in with your email and password</span>
+
+            <form onSubmit={this.handleSubmit}>
+                <FormInput
+                    name='email'
+                    type='email'
+                    handleChange={this.handleChange}
+                    value={this.state.email}
+                    label='email'
+                    required
+                />
+                <FormInput
+                    name='password'
+                    type='password'
+                    value={this.state.password}
+                    handleChange={this.handleChange}
+                    label='password'
+                    required
+                />
+                <ButtonsBarContainer>
+                    <CustomButton type='submit'> Sign in </CustomButton>
+                    <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                        Sign in with Google
+        </CustomButton>
+                </ButtonsBarContainer>
+            </form>
+        </SignInContainer>
+    );
+}
+
+export  default Register;
